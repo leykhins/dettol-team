fa099e3af608050c140b643be018c686445324ca
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2020-07-13 10:26:08 +0100

    Trying out

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from urllib.parse import urljoin
 from PIL import Image, ImageFont, ImageDraw
 from flask import Flask, request, url_for, redirect
-from flask import render_template
+from flask import render_template, send_file
 
 app = Flask(__name__)
 BASE_PATH = os.path.dirname(__file__)
@@ -15,11 +15,6 @@ GENERATED_PATH = os.path.join(STATIC_PATH, "generated")
 @app.route("/")
 def index():
     return render_template('index.html')
-
-if __name__ == '__main__':
-   app.run()
-#def index():
-#    return "Hello World"
 
 @app.route("/generate/")
 def generate():
@@ -74,68 +69,20 @@ def make_certificate(username, number):
     base_64 =  urljoin(request.host_url, url_for("static", filename="generated/" + img_title))
 
     return base_64
-# def make_certificate(username, jerseynumber, type, track=None):
-#     def draw_text(filename, type, username, jerseynumber, track=None):
-#         font = "PTSans-Bold.ttf"
-#         color = "#ff0000"
-#         size = 50
-#         track_color = "#000000"
-#         track_size = 20
-#         y = 350
-#         x = 0
-#         text = "{} {}".format(username, jerseynumber).upper()
-#         if type == "2":
-#             font = "LeagueSpartan-Bold.otf"
-#             size = 60
-#             color = "#e05a47"
-#             y = 175
-#             x = 88
-#             text = "{}\n{}".format(username, jerseynumber).upper()
-#         raw_img = Image.open(os.path.join("certificates", filename))
-#         img = raw_img.copy()
-#         draw = ImageDraw.Draw(img)
 
-#         # draw name
-#         if username:
-#             PIL_font = ImageFont.truetype(os.path.join("fonts", font), size)
-#             w, h = draw.textsize(text, font=PIL_font)
-#             W, H = img.size
-#             x = (W - w) / 2 if x == 0 else x
-#             draw.text((x, y), text, fill=color, font=PIL_font)
-#         img_url = os.path.join("static", "{}-{}-Join-Me-On-The-Cool-Team.png".format(username, jerseynumber, type))
-#         img.save(img_url)
-#         return request.host_url + img_url
+# @app.route('/file-downloads/')
+# def file_downloads():
+#     try:
+#         return render_template('downloads.html')
+#     except Exception as e:
+#         return str(e)
 
-#2
-        # draw track
-        #if track:
-        #    PIL_font = ImageFont.truetype(os.path.join("fonts", font), track_size)
-        #    w, h = draw.textsize(track, font=PIL_font)
-        #    x, y = 183, 450
-        #    draw.text((x, y), track, fill=track_color, font=PIL_font)
-        #img_url = os.path.join("static", "{}-{}-{}-Join-Me-On-The-Winning-Team.png".format(username, jerseynumber, type))
-        #img.save(img_url)
-        #return request.host_url + img_url
-
-#new code
-    # base_64 = draw_text("framed.png", type, username, jerseynumber)
-    # return base_64
-
-    #tracks = {"frontend": "Front-End Web Development", "backend": "Back-End Web Development", "python": "Python Programming", "android": "Mobile Development", "ui": "UI/UX Design", "design": "Engineering Design"}
-    #track = tracks.get(track, None)
-    #    base_64 = draw_text("framed.png", type, first_name, last_name, track)
-    #if type == "3":
-    #    base_64 = draw_text("average performace.jpg", type, first_name, last_name, track)
-    #if type == "4":
-    #    base_64 = draw_text("good performance.jpg", type, first_name, last_name, track)
-    #if type == "5":
-    #    base_64 = draw_text("outstanding.jpg", type, first_name, last_name, track)
-    #if type == "1":
-    #    base_64 = draw_text("participated.jpg", type, first_name, last_name)
-    #if type == "2":
-    #    base_64 = draw_text("mentor.jpg", type, first_name, last_name)
-    #return base_64
-
+# @app.route('/return-files/')
+# def return_files_tut():
+# 	try:
+# 		return send_file('/static/images/python.jpg', attachment_filename='team.jpg')
+# 	except Exception as e:
+# 		return str(e)
 
 if __name__ == "__main__":
     app.run(debug=True)
