975309a18d55f84b7c278911c1edc93fa523d7ca
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2020-07-20 21:55:16 +0100

    Made preview feature

--- app.py
+++ app.py
@@ -15,23 +15,33 @@ FONT_PATH = os.path.join(STATIC_PATH, "fonts")
 CERTIFICATE_PATH = os.path.join(STATIC_PATH, "certificates")
 GENERATED_PATH = os.path.join(STATIC_PATH, "generated")
 
-@app.route("/", methods={'GET', 'POST'})
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        username, number = request.form.get('username'), request.form.get('number')
+    if request.method == 'POST':
+        username, number = request.form.get(
+            'username'), request.form.get('number')
         write_to_csv(request.form)
         certificate = make_certificate(username, number)
-    return send_file(certificate, as_attachment=True, mimetype='image.png', attachment_filename='team.png')
+        return render_template("download.html", certificate=certificate)
+    return render_template('index.html')
+
+
+@app.route("/download-certificate/")
+def download_page():
+    certificate_path = request.args.get("certificate_path")
+    return send_file(certificate_path, as_attachment=True, mimetype='image.png', attachment_filename='team.png')
+
 
 @app.route("/generate/")
 def generate():
     certificate = make_certificate(**request.args)
     return redirect(certificate)
 
+
 def delete_file(img_title):
     os.unlink(os.path.join(GENERATED_PATH, img_title))
+
 
 def make_certificate(username, number):
     # set certificate style
@@ -57,12 +67,12 @@ def make_certificate(username, number):
     PIL_font = ImageFont.truetype(os.path.join(FONT_PATH, font), size)
     w, h = draw.textsize(text, font=PIL_font)
     W, H = img.size
-    print(w)
-    x = 382.603000431 +  ((272.780888595 - w) / 2)
+    x = 382.603000431 + ((272.780888595 - w) / 2)
     draw.text((x, y), text, fill=color, font=PIL_font)
 
     # draw number
-    PIL_font = ImageFont.truetype(os.path.join(FONT_PATH, track_font), track_size)
+    PIL_font = ImageFont.truetype(
+        os.path.join(FONT_PATH, track_font), track_size)
     track_text = "{}".format(number)
     w, h = draw.textsize(track_text, font=PIL_font)
     W, H = img.size
@@ -78,6 +88,7 @@ def make_certificate(username, number):
     #base_64 = urljoin(request.host_url, url_for("static", filename="generated/" + img_title))
     return os.path.join(GENERATED_PATH, img_title)
 
+
 def download():
     try:
         image = request.files['img_title']
@@ -89,19 +100,23 @@ def download():
         print(e)
         return redirect(url_for('upload'))
 
+
 def write_to_csv(data):
     with open('database.csv', mode='a') as database:
         username = data['username']
         number = data['number']
         email = data['email']
         phone_number = data['phone-number']
-        csv_write = csv.writer(database, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_write = csv.writer(database, quotechar='"',
+                               quoting=csv.QUOTE_MINIMAL)
         csv_write.writerow([username, number, email, phone_number])
+
 
 # handling error 404 - Page not found
 @app.errorhandler(404)
 def page_not_found(error):
-   return render_template('404.html', title='404'), 404
+    return render_template('404.html', title='404'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
